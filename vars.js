4a51c36651f5ec4294d558a8577919118c4d1162
Ike The Coder <ikethecoder@canzea.com>
2017-10-30 19:47:35 -0700

    Implemented CONSUL and VAULT integration

--- vars.js
+++ vars.js
@@ -14,31 +14,145 @@
  * limitations under the License.
  **/
 
+var request = require('request');
+var async = require('async');
+
 module.exports = function(RED) {
     "use strict";
 
-    var operators = {
-        'eq': function(a, b) { return a == b; },
-        'neq': function(a, b) { return a != b; },
-        'lt': function(a, b) { return a < b; },
-        'lte': function(a, b) { return a <= b; },
-        'gt': function(a, b) { return a > b; },
-        'gte': function(a, b) { return a >= b; },
-        'btwn': function(a, b, c) { return a >= b && a <= c; },
-        'cont': function(a, b) { return (a + "").indexOf(b) != -1; },
-        'regex': function(a, b, c, d) { return (a + "").match(new RegExp(b,d?'i':'')); },
-        'true': function(a) { return a === true; },
-        'false': function(a) { return a === false; },
-        'null': function(a) { return (typeof a == "undefined" || a === null); },
-        'nnull': function(a) { return (typeof a != "undefined" && a !== null); },
-        'else': function(a) { return a === true; }
-    };
+
+    function doRequest (node, msg, url, tlsNode, callback) {
+
+        var opts = {
+            method: 'GET',
+            timeout: node.reqTimeout,
+            followRedirect: false,
+            headers: {
+                "X-Vault-Token": process.env.VAULT_TOKEN
+            }
+        };
+
+        var rule = url.rule;
+
+        if (rule.t == "keyvalue") {
+            opts['url'] = url.url + "/v1/kv/" + rule.v;
+        } else if (rule.t == "service") {
+            opts['url'] = url.url + "/v1/catalog/service/" + rule.v;
+        } else {
+            opts['url'] = url.url + "/v1/secret/" + rule.v;
+        }
+
+        node.debug("CALLING: " + opts['url']);
+
+        if (tlsNode) {
+            tlsNode.addTLSOptions(opts);
+        }
+
+        return request(opts, function (error, response, body) {
+            node.status({});
+
+            if (error) {
+              if (error.code === 'ETIMEDOUT') {
+                node.error(RED._("common.notification.errors.no-response"), msg);
+                setTimeout(function () {
+                  node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "common.notification.errors.no-response"
+                  });
+                }, 10);
+              } else {
+                node.error(error, msg);
+                //msg.payload = error.toString() + " : " + url;
+                //msg.statusCode = error.code;
+                node.send(msg);
+                node.status({
+                  fill: "red",
+                  shape: "ring",
+                  text: error.code
+                });
+              }
+            } else {
+
+              if (node.metric()) {
+                // Calculate request time
+                var diff = process.hrtime(preRequestTimestamp);
+                var ms = diff[0] * 1e3 + diff[1] * 1e-6;
+                var metricRequestDurationMillis = ms.toFixed(3);
+                node.metric("duration.millis", msg, metricRequestDurationMillis);
+                if (response.connection && response.connection.bytesRead) {
+                  node.metric("size.bytes", msg, response.connection.bytesRead);
+                }
+              }
+
+              if (response.statusCode != 200) {
+                  msg.missing.push(rule.v);
+              } else {
+                  if (rule.t == "keyvalue") {
+
+                      var key = rule.v.toUpperCase();
+                      key = key.replace(/\./g, '').replace(/-/g, '_').replace(/\//g, '_');
+                      var data = JSON.parse(body)[0]['Value'];
+
+                      var contents = new Buffer(data, 'base64').toString('utf8')
+
+                      msg.envs[key] = contents;
+
+                      node.debug("Result = "+contents);
+
+                  } else if (rule.t == "service") {
+
+                        var name = rule.v.toUpperCase();
+
+                        var services = JSON.parse(body);
+
+                        services.forEach(function (s) {
+
+                            var key = "" + name + "_ADDRESS"
+                            key = key.replace(/\./g, '').replace(/-/g, '_').replace(/\//g, '_');
+                            node.debug("Setting: " + key + " : " + s["ServiceAddress"])
+                            msg.envs[key] = s["ServiceAddress"];
+
+                            key = "" + name + "_PORT"
+                            key = key.replace(/\./g, '').replace(/-/g, '_').replace(/\//g, '_');
+                            node.debug("Setting: " + key + " : " + String(s["ServicePort"]))
+                            msg.envs[key] = "" + s["ServicePort"];
+
+                            key = "" + name + "_URL"
+                            key = key.replace(/\./g, '').replace(/-/g, '_').replace(/\//g, '_');
+                            var val = "http://" + s["ServiceAddress"] + ":" + String(s["ServicePort"])
+                            node.debug("Setting: " + key + " : " + val)
+                            msg.envs[key] = val;
+                        });
+
+                  } else {
+                      var data = JSON.parse(body)['data'];
+                      var key = rule.v.toUpperCase();
+                      key = key.replace(/\./g, '').replace(/-/g, '_').replace(/\//g, '_');
+
+                      Object.keys(data).forEach(function (e) {
+                          var fkey = e.toUpperCase().replace(/\./g, '').replace(/-/g, '_').replace(/\//g, '_');
+
+                          msg.envs[key + "_" + fkey] = data[e];
+                      });
+                  }
+              }
+              //node.warn("Finished request for " + rule.v);
+            }
+            callback();
+        });
+
+    }
 
     function VarsNode(n) {
         RED.nodes.createNode(this, n);
         this.rules = n.rules || [];
         this.property = n.property;
         this.propertyType = n.propertyType || "msg";
+
+        if (n.tls) {
+          var tlsNode = RED.nodes.getNode(n.tls);
+        }
 
         if (this.propertyType === 'jsonata') {
             try {
@@ -100,67 +214,36 @@ module.exports = function(RED) {
         }
 
         this.on('input', function (msg) {
-            var onward = [];
+            node.status({
+                fill: "blue",
+                shape: "dot",
+                text: "Requesting"
+            });
+
+            var promises = [];
             try {
-                var prop;
-                if (node.propertyType === 'jsonata') {
-                    prop = RED.util.evaluateJSONataExpression(node.property,msg);
-                } else {
-                    prop = RED.util.evaluateNodeProperty(node.property,node.propertyType,node,msg);
-                }
-                var elseflag = true;
+                msg.envs = {};
+                msg.missing = [];
+
+                var urls = [];
                 for (var i=0; i<node.rules.length; i+=1) {
                     var rule = node.rules[i];
-                    var test = prop;
-                    var v1,v2;
-                    if (rule.vt === 'prev') {
-                        v1 = node.previousValue;
-                    } else if (rule.vt === 'jsonata') {
-                        try {
-                            v1 = RED.util.evaluateJSONataExpression(rule.v,msg);
-                        } catch(err) {
-                            node.error(RED._("vars.errors.invalid-expr",{error:err.message}));
-                            return;
-                        }
-                    } else {
-                        try {
-                            v1 = RED.util.evaluateNodeProperty(rule.v,rule.vt,node,msg);
-                        } catch(err) {
-                            v1 = undefined;
-                        }
-                    }
-                    v2 = rule.v2;
-                    if (rule.v2t === 'prev') {
-                        v2 = node.previousValue;
-                    } else if (rule.v2t === 'jsonata') {
-                        try {
-                            v2 = RED.util.evaluateJSONataExpression(rule.v2,msg);
-                        } catch(err) {
-                            node.error(RED._("vars.errors.invalid-expr",{error:err.message}));
-                            return;
-                        }
-                    } else if (typeof v2 !== 'undefined') {
-                        try {
-                            v2 = RED.util.evaluateNodeProperty(rule.v2,rule.v2t,node,msg);
-                        } catch(err) {
-                            v2 = undefined;
-                        }
-                    }
-                    if (rule.t == "else") { test = elseflag; elseflag = true; }
-                    if (operators[rule.t](test,v1,v2,rule.case)) {
-                        onward.push(msg);
-                        elseflag = false;
-                        if (node.checkall == "false") { break; }
-                    } else {
-                        onward.push(null);
-                    }
+
+                    urls.push({"url":n.url, "rule":rule});
                 }
-                node.previousValue = prop;
-                this.send(onward);
+
+                async.each(urls, function (url, callback) {
+                    doRequest (node, msg, url, tlsNode, callback);
+                }, function (err) {
+                    node.send(msg);
+                });
             } catch(err) {
                 node.warn(err);
             }
         });
+
+
+
     }
     RED.nodes.registerType("vars", VarsNode);
 }
